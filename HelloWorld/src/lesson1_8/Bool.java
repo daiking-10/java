65b6273f49415a50cf6e1616ae8b493903f33f84
moridaiki <moridaiki@192.168.11.41>
2023-04-03 21:24:25 +0900

    真偽値と条件式_1_8(修正後)

--- HelloWorld/src/lesson1_8/Bool.java
+++ HelloWorld/src/lesson1_8/Bool.java
@@ -7,7 +7,7 @@ public class Bool {
 //		boolean型変数 result_1 をコンソールに出力して下さい。
 		int a = 20;
 		int b = 5;
-		boolean result_1 = (a > b);
+		boolean result_1 = (a >= b);
 		System.out.println(result_1);
 //		問２）
 //		以下の変数を使ってboolean型変数 result_2 に「変数 c の値と変数 d の値は等しくない」という条件式を代入して下さい。
